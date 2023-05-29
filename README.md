a1b7488351241d074f9cd8543c6f51853ba6db2c
kushuaiming <xm2534911078@outlook.com>
2023-05-29 23:10:06 +0800

    find first and last position of element in sorted array

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LeetCode
 The algorithms form [Leetcode](https://leetcode.cn/).
+## 代码编写
+### 命名
+返回的结果可以命名为result或者answer
+## 算法
+### 二分查找Binary Search
+- 需要明确区间的开闭性, 左闭右开(满足C++/Python的语言习惯)或者左闭右闭(便于处理边界条件).
---
+++ binary_search/34_find_first_and_last_position_of_element_in_sorted_array.cc
@@ -0,0 +1,39 @@
+// 二分查找
+// 寻找left_index即为在数组中寻找第一个大于等于target的下标
+// 寻找right_index即为在数组中寻找第一个大于target的下标, 然后将下标减1
+#include <iostream>
+#include <limits>
+#include <vector>
+
+int BinarySearch(std::vector<int>& nums, int target, bool lower) {
+  int left = 0;
+  int right = static_cast<int>(nums.size()) - 1;
+  int answer = static_cast<int>(nums.size());
+  while (left <= right) {
+    int mid = (left + right) / 2;
+    if (nums[mid] > target || (lower && nums[mid] >= target)) {
+      right = mid - 1;
+      answer = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return answer;
+}
+
+std::vector<int> SearchRange(std::vector<int>& nums, int target) {
+  int left_index = BinarySearch(nums, target, true);
+  int right_index = BinarySearch(nums, target, false) - 1;
+  if (left_index <= right_index && right_index < nums.size() &&
+      nums[left_index] == target && nums[right_index] == target) {
+    return std::vector<int>{left_index, right_index};
+  }
+  return std::vector<int>{-1, -1};
+}
+
+int main(int argc, char* argv[]) {
+  std::vector<int> nums{5, 7, 7, 8, 8, 10};
+  std::vector<int> result = SearchRange(nums, 8);
+  std::cout << result[0] << ' ' << result[1] << std::endl;
+  return 0;
+}
