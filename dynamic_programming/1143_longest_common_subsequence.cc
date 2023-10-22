624715f7339dd0a031e73079a34ce939d75643d7
kushuaiming <xm2534911078@outlook.com>
2023-10-22 20:55:24 +0800

    feat: 1143 longest common subsequence

---
+++ dynamic_programming/1143_longest_common_subsequence.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int LongestCommonSubsequence(const std::string& text1,
+                             const std::string& text2) {
+  const int m = text1.size();
+  const int n = text2.size();
+  std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, 0));
+  for (int i = 1; i <= m; ++i) {
+    for (int j = 1; j <= n; ++j) {
+      if (text1[i - 1] == text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
+      }
+      std::cout << "dp[" << i << "][" << j << "] = " << dp[i][j] << std::endl;
+    }
+  }
+  return dp[m][n];
+}
+
+int main(int argc, char* argv[]) {
+  std::string text1 = "abcde";
+  std::string text2 = "ace";
+  std::cout << LongestCommonSubsequence(text1, text2) << std::endl;
+  return 0;
+}
