b34dd09a353e4634037999547e7c866426398823
kushuaiming <xm2534911078@outlook.com>
2023-12-17 10:44:50 +0800

    feat: 155_min_stack

---
+++ data_structure/stack_and_queue/155_min_stack.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <limits>
+#include <stack>
+
+class MinStack {
+ public:
+  MinStack() { min_stack_.push(std::numeric_limits<int>::max()); }
+
+  void push(int val) {
+    x_stack_.push(val);
+    min_stack_.push(std::min(val, min_stack_.top()));
+  }
+
+  void pop() {
+    x_stack_.pop();
+    min_stack_.pop();
+  }
+
+  int top() { return x_stack_.top(); }
+
+  int getMin() { return min_stack_.top(); }
+
+ private:
+  std::stack<int> x_stack_;
+  std::stack<int> min_stack_;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+int main(int argc, char* argv[]) {
+  MinStack* obj = new MinStack();
+  obj->push(-2);
+  obj->push(-1);
+  obj->push(-1);
+  obj->pop();
+  int param_3 = obj->top();
+  int param_4 = obj->getMin();
+  std::cout << "param_3: " << param_3 << ", param_4: " << param_4 << std::endl;
+  return 0;
+}
