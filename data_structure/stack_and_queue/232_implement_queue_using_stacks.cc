ca131b71d78b618b9f9fdc43d46cd0c8aa9be746
kushuaiming <xm2534911078@outlook.com>
2023-12-17 10:12:49 +0800

    feat: 232_implement_queue_using_stacks

---
+++ data_structure/stack_and_queue/232_implement_queue_using_stacks.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <stack>
+
+// 输入栈 & 输出栈
+// 要通过一个额外栈翻转一次数组
+class MyQueue {
+ public:
+  MyQueue() {}
+
+  void push(int x) { in_stack_.push(x); }
+
+  int pop() {
+    if (out_stack_.empty()) {
+      while (!in_stack_.empty()) {
+        out_stack_.push(in_stack_.top());
+        in_stack_.pop();
+      }
+    }
+    int x = out_stack_.top();
+    out_stack_.pop();
+    return x;
+  }
+
+  int peek() {
+    if (out_stack_.empty()) {
+      while (!in_stack_.empty()) {
+        out_stack_.push(in_stack_.top());
+        in_stack_.pop();
+      }
+    }
+    return out_stack_.top();
+  }
+
+  bool empty() { return in_stack_.empty() && out_stack_.empty(); }
+
+ private:
+  std::stack<int> in_stack_;
+  std::stack<int> out_stack_;
+};
+
+int main(int argc, char* argv[]) {
+  int x = 10;
+  MyQueue* obj = new MyQueue();
+  obj->push(x);
+  int param_3 = obj->peek();
+  int param_2 = obj->pop();
+  bool param_4 = obj->empty();
+  std::cout << "param_2: " << param_2 << ", param_3: " << param_3
+            << ", param_4: " << param_4 << std::endl;
+  return 0;
+}
