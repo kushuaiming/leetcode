b84e122b3b970666f4a930538ae2d2a078b9a4e9
kushuaiming <xm2534911078@outlook.com>
2023-12-18 22:15:18 +0800

    feat: 1_two_sum

---
+++ data_structure/hash_table/1_two_sum.cc
@@ -0,0 +1,13 @@
+#include <unordered_map>
+#include <vector>
+
+std::vector<int> TwoSum(std::vector<int>& nums, int target) {
+  std::unordered_map<int, int> hash;
+  for (int i = 0; i < static_cast<int>(nums.size()); ++i) {
+    if (hash.find(target - nums[i]) != hash.end()) {
+      return {i, hash[target - nums[i]]};
+    }
+    hash[nums[i]] = i; // 关键是key-value的设计.
+  }
+  return {};
+}
