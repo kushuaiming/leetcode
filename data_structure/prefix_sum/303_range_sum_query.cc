6745465896824f97940320e27c83b6b9fd7fc022
Ming Xu <ming.xu@allride.ai>
2023-12-19 17:09:54 +0800

    feat: 303_range_sum_query

---
+++ data_structure/prefix_sum/303_range_sum_query.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+class NumArray {
+ public:
+  NumArray(std::vector<int>& nums) : prefix_sum_(nums.size() + 1, 0) {
+    std::partial_sum(nums.begin(), nums.end(), prefix_sum_.begin() + 1);
+  }
+
+  int SumRange(int left, int right) {
+    return prefix_sum_[right + 1] - prefix_sum_[left];
+  }
+
+ private:
+  std::vector<int> prefix_sum_;
+};
+
+int main(int argc, char* argv[]) {
+  std::vector<int> nums = {-2, 0, 3, -5, 2, -1};
+  NumArray* obj = new NumArray(nums);
+  const int param_1 = obj->SumRange(0, 2);
+  std::cout << param_1 << std::endl;
+  return 0;
+}
