3d541671b3bbd0fd14e54598169a9fbdcaa1c376
Ming Xu <ming.xu@allride.ai>
2023-12-20 16:57:34 +0800

    feat: 227_basic_calculator_II

---
+++ data_structure/stack_and_queue/227_basic_calculator_II.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+
+int Calculate(const std::string& s) {
+  std::vector<int> stk;
+  char pre_sign = '+';
+  int num = 0;
+  const int n = s.size();
+  for (int i = 0; i < n; ++i) {
+    if (std::isdigit(s[i])) {
+      num = num * 10 + static_cast<int>(s[i] - '0');
+    }
+    if (!std::isdigit(s[i]) && s[i] != ' ' || i == n - 1) {
+      switch (pre_sign) {
+        case '+': {
+          stk.push_back(num);
+          break;
+        }
+        case '-': {
+          stk.push_back(-num);
+          break;
+        }
+        case '*': {
+          stk.back() *= num;
+          break;
+        }
+        case '/': {
+          stk.back() /= num;
+          break;
+        }
+        default:
+          break;
+      }
+      pre_sign = s[i];
+      num = 0;
+    }
+  }
+  return std::accumulate(stk.begin(), stk.end(), 0);
+}
+
+int main(int argc, char* argv[]) {
+  const std::string s = "3+2*2";
+  const std::string s2 = "42";
+  std::cout << Calculate(s2) << std::endl;
+  return 0;
+}
