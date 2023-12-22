2582ad550221b2df67f548b8731568e4d7d3dcc2
Ming Xu <ming.xu@allride.ai>
2023-12-22 14:43:24 +0800

    feat: 99_recover_binary_search_tree

---
+++ tree/binary_search_tree/99_recover_binary_search_tree.cc
@@ -0,0 +1,73 @@
+#include <vector>
+
+#include "tree_node.h"
+
+// 关键在于理解在一个递增序列中交换两个值, 会造成一个或者两个位置不满足升序条件.
+void RecoverTree(TreeNode* root) {
+  TreeNode* mistake1 = nullptr;
+  TreeNode* mistake2 = nullptr;
+  TreeNode* prev = nullptr;
+  Inorder(root, mistake1, mistake2, prev);
+  if (mistake1 && mistake2) {
+    const int temp = mistake1->val;
+    mistake1->val = mistake2->val;
+    mistake2->val = temp;
+  }
+}
+
+// 隐式中序遍历
+void Inorder(TreeNode* root, TreeNode* mistake1, TreeNode* mistake2,
+             TreeNode* prev) {
+  if (root == nullptr) {
+    return;
+  }
+  if (root->left) {
+    Inorder(root->left, mistake1, mistake2, prev);
+  }
+  if (prev && root->val < prev->val) {
+    if (mistake1 == nullptr) {
+      mistake1 = prev;
+      mistake2 = root;
+    } else {
+      mistake2 = root;
+    }
+  }
+  prev = root;
+  if (root->right) {
+    Inorder(root->right, mistake1, mistake2, prev);
+  }
+}
+
+
+void RecoverTree2(TreeNode* root) {
+  std::vector<TreeNode*> inorder_results;
+  Inorder2(root, inorder_results);
+  std::vector<TreeNode*> mistakes;
+  for (int i = 0; i < inorder_results.size() - 1; ++i) {
+    if (inorder_results[i]->val > inorder_results[i + 1]->val) {
+      if (mistakes.empty()) {
+        mistakes.push_back(inorder_results[i]);
+        mistakes.push_back(inorder_results[i + 1]);
+      } else {
+        mistakes.push_back(inorder_results[i + 1]);
+      }
+    }
+  }
+  TreeNode* mistake1 = mistakes.front();
+  TreeNode* mistake2 = mistakes.back();
+  if (mistake1 && mistake2) {
+    const int temp = mistake1->val;
+    mistake1->val = mistake2->val;
+    mistake2->val = temp;
+  }
+}
+
+// 显式中序遍历
+void Inorder2(TreeNode* root, std::vector<TreeNode*>& inorder_results) {
+  if (root == nullptr) {
+    return;
+  }
+  Inorder2(root->left, inorder_results);
+  inorder_results.push_back(root);
+  Inorder2(root->right, inorder_results);
+}
