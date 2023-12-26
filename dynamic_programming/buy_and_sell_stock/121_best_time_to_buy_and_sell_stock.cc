7183d6e37508f068b07ff4cbf9a2dab28f74e62a
Ming Xu <ming.xu@allride.ai>
2023-12-26 13:48:38 +0800

    feat: 121_best_time_to_buy_and_sell_stock

--- dynamic_programming/buy_and_sell_stock/121_best_time_to_buy_and_sell_stock.cc
+++ dynamic_programming/buy_and_sell_stock/121_best_time_to_buy_and_sell_stock.cc
@@ -1,10 +1,11 @@
 #include <algorithm>
+#include <iostream>
 #include <limits>
 #include <vector>
 
 // 把自己代入一个: 可以每天观察和记录收益情况和可以无限反悔重买的情景
 // 关键是记录历史最低价
-int maxProfit(const std::vector<int>& prices) {
+int MaxProfit(const std::vector<int>& prices) {
   int history_min_price = std::numeric_limits<int>::max();
   int max_profit = 0;
   for (int i = 0; i < static_cast<int>(prices.size()); ++i) {
@@ -12,4 +13,10 @@ int maxProfit(const std::vector<int>& prices) {
     history_min_price = std::min(history_min_price, prices[i]);
   }
   return max_profit;
+}
+
+int main(int argc, char* argv[]) {
+  std::vector<int> prices = {7, 1, 5, 3, 6, 4};
+  std::cout << MaxProfit(prices) << std::endl;
+  return 0;
 }
