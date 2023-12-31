86e049ff53ce3387cd47f061bc0b3916e1929d10
kushuaiming <xm2534911078@outlook.com>
2023-12-31 15:38:11 +0800

    feat: 332_reconstruct_itinerary

--- data_structure/hash_table/332_reconstruct_itinerary.cc
+++ data_structure/hash_table/332_reconstruct_itinerary.cc
@@ -6,21 +6,24 @@
 #include <unordered_map>
 #include <vector>
 
+// 关键1: 用哈希表记录起止机场, 其中 key 是起始机场, value 是一个多重集合, 表示对应的终止机场.
+// 因为一个人可能坐过重复的线路, 所以我们需要使用多重集合存储重复值.
+// 关键2: 存储完成之后, 我们可以利用栈来恢复从终点到起点飞行的顺序.
+// 关键3: 最后将结果逆序得到从起点到终点的顺序.
 std::vector<std::string> FindItinerary(
-    std::vector<std::vector<std::string>>& tickets) {
-  // key: start point, value: multi end point.
+    const std::vector<std::vector<std::string>>& tickets) {
   std::unordered_map<std::string, std::multiset<std::string>> hash_table;
   for (const auto& ticket : tickets) {
     hash_table[ticket[0]].insert(ticket[1]);
   }
 
-  std::vector<std::string> ans;
+  std::vector<std::string> res;
   std::stack<std::string> s;
   s.push("JFK");
   while (!s.empty()) {
     const std::string next = s.top();
     if (hash_table[next].empty()) {
-      ans.emplace_back(next);
+      res.emplace_back(next);
       s.pop();
     } else {
       s.push(*hash_table[next].begin());
@@ -28,10 +31,9 @@ std::vector<std::string> FindItinerary(
     }
   }
 
-  // 将结果逆序得到从起点到终点的顺序.
-  std::reverse(ans.begin(), ans.end());
+  std::reverse(res.begin(), res.end());
 
-  return ans;
+  return res;
 }
 
 int main(int argc, char* argv[]) {
@@ -44,8 +46,8 @@ int main(int argc, char* argv[]) {
                                                     {"ATL", "SFO"}};
   std::vector<std::vector<std::string>> tickets3 = {
       {"JFK", "KUL"}, {"JFK", "NRT"}, {"NRT", "JFK"}};
-  std::vector<std ::string> ans = FindItinerary(tickets3);
-  for (const std::string& ticket : ans) {
+  std::vector<std ::string> res = FindItinerary(tickets3);
+  for (const std::string& ticket : res) {
     std::cout << ticket << std::endl;
   }
   return 0;
