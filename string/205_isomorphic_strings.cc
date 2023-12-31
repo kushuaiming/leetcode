bdc1dba635700d64785a76f740489ef4cdf53c07
kushuaiming <xm2534911078@outlook.com>
2023-12-31 17:23:37 +0800

    docs: 205_isomorphic_strings

--- string/205_isomorphic_strings.cc
+++ string/205_isomorphic_strings.cc
@@ -2,6 +2,9 @@
 #include <string>
 #include <vector>
 
+// 记录两个字符串每个位置字符第一次(或上一次)出现的位置,
+// 如果两个字符串相同位置的字符与它们第一次(或上一次)出现的位置一样,
+// 那么这两个字符串同构.
 bool IsIsomorphic(const std::string& s, const std::string& t) {
   // 初始化的值不能是index可能会出现的数.
   std::vector<int> s_index(256, -1);
