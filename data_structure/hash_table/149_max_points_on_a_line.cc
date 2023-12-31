d02f87182b48c345bb2760955a53e20ca684493d
kushuaiming <xm2534911078@outlook.com>
2023-12-31 14:55:07 +0800

    feat: 149_max_points_on_a_line

--- data_structure/hash_table/149_max_points_on_a_line.cc
+++ data_structure/hash_table/149_max_points_on_a_line.cc
@@ -1,7 +1,10 @@
 #include <unordered_map>
 #include <vector>
 
-int MaxPoints(std::vector<std::vector<int>>& points) {
+// 一条线可以由一个点和斜率而唯一确定.
+// 对于每个点, 我们对其它点建立哈希表, key 设计为斜率, value 设计为同一斜率的点的数量.
+// 另外也要考虑斜率不存在和重复坐标的情况.
+int MaxPoints(const std::vector<std::vector<int>>& points) {
   std::unordered_map<double, int> hash;
   int max_count = 0;
   int same_x = 0;
@@ -20,7 +23,7 @@ int MaxPoints(std::vector<std::vector<int>>& points) {
     for (auto item : hash) {
       max_count = std::max(max_count, item.second + 1);
     }
-    hash.clear(); // 不要忘记clear
+    hash.clear();  // 不要忘记clear
   }
   return max_count;
 }
