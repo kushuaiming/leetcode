8671edb502cdcf54e55023dc96f7a83a17b452d6
kushuaiming <xm2534911078@outlook.com>
2023-12-31 11:01:06 +0800

    feat: 1_two_sum

--- data_structure/hash_table/1_two_sum.cc
+++ data_structure/hash_table/1_two_sum.cc
@@ -1,6 +1,7 @@
 #include <unordered_map>
 #include <vector>
 
+// 2023/12/31 可以自己写出来
 std::vector<int> TwoSum(std::vector<int>& nums, int target) {
   std::unordered_map<int, int> hash;
   for (int i = 0; i < static_cast<int>(nums.size()); ++i) {
