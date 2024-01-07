b9f02a4b8bb24d207b3bf90f8f12fde41fb2f55b
kushuaiming <xm2534911078@outlook.com>
2024-01-07 23:12:27 +0800

    feat: 138_copy_list_with_random_pointer

--- linked_list/138_copy_list_with_random_pointer.cc
+++ linked_list/138_copy_list_with_random_pointer.cc
@@ -30,3 +30,41 @@ Node* copyRandomList(Node* head) {
   }
   return hash_map[head];
 }
+
+// 2024/01/07: 直接抄答案, 回溯 + 哈希表
+std::unordered_map<Node*, Node*> cached_node_;
+Node* CopyRandomList2(Node* head) {
+  if (head == nullptr) {
+    return nullptr;
+  }
+  if (!cached_node_.count(head)) {
+    Node* head_new = new Node(head->val);
+    cached_node_[head] = head_new;
+    head_new->next = CopyRandomList2(head->next);
+    head_new->random = CopyRandomList2(head->random);
+  }
+  return cached_node_[head];
+}
+
+// 2024/01/07: 直接抄答案, 迭代 + 节点拆分
+Node* CopyRandomList(Node* head) {
+  if (head == nullptr) {
+    return nullptr;
+  }
+  for (Node* node = head; node != nullptr; node = node->next->next) {
+    Node* node_new = new Node(node->val);
+    node_new->next = node->next;
+    node->next = node_new;
+  }
+  for (Node* node = head; node != nullptr; node = node->next->next) {
+    Node* node_new = node->next;
+    node_new->random = node->random ? node->random->next : nullptr;
+  }
+  Node* head_new = head->next;
+  for (Node* node = head; node != nullptr; node = node->next) {
+    Node* node_new = node->next;
+    node->next = node->next->next;
+    node_new->next = node_new->next ? node_new->next->next : nullptr;
+  }
+  return head_new;
+}
