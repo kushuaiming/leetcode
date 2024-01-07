2b20d798f0742d36f2637dff4030038ce5cc8012
kushuaiming <xm2534911078@outlook.com>
2024-01-07 15:07:14 +0800

    feat: randomized

--- math/randomized/384_shuffle_an_array.cc
+++ math/randomized/384_shuffle_an_array.cc
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-
 #include <iostream>
 #include <vector>
 
@@ -18,8 +16,9 @@ class Solution {
     const int n = origin_.size();
     // 正向洗牌
     for (int i = 0; i < n; ++i) {
-      const int pos = rand() % (n - i);
-      std::swap(shuffled[i], shuffled[i + pos]);
+      // 关键公式: 每次从剩余的牌中([i, n)的下标中)选一个放在i的位置.
+      const int j = i + rand() % (n - i);
+      std::swap(shuffled[i], shuffled[j]);
     }
     // // 或者反向洗牌
     // for (int i = n - 1; i >= 0; --i) {
