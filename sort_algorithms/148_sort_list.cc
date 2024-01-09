1cbcb18462a6fd9edee4d21bbb46c6b272a55202
Ming Xu <ming.xu@allride.ai>
2024-01-09 17:45:35 +0800

    feat: 148_sort_list

--- sort_algorithms/148_sort_list.cc
+++ sort_algorithms/148_sort_list.cc
@@ -1,3 +1,6 @@
+#include <vector>
+#include <queue>
+
 struct ListNode {
   int val;
   ListNode* next;
@@ -6,34 +9,34 @@ struct ListNode {
   ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
+// 2024/01/09: 自己实现, 堆排序
+ListNode* SortList(ListNode* head) {
+  auto cmp = [](ListNode* left, ListNode* right) {
+    return left->val > right->val;
+  };
+  std::priority_queue<ListNode*, std::vector<ListNode*>, decltype(cmp)> q(cmp);
+  ListNode* temp = head;
+  while (head) {
+    q.push(head);
+    head = head->next;
+  }
+  ListNode* dummy_node = new ListNode(0);
+  ListNode* curr = dummy_node;
+  while (!q.empty()) {
+    curr->next = q.top();
+    q.pop();
+    curr = curr->next;
+  }
+  curr->next = nullptr;
+  return dummy_node->next;
+}
+
 // 2024/01/08: 直接抄答案.
 // O(nlogn)的排序算法中, 最适合链表的是归并排序.
 // 自顶向下归并排序.
 // 1. 找到链表中点, 以中点为分界, 将链表拆分成两个子链表.
 // 2. 对两个子链表分别排序
 // 3. 将两个排序后的子链表合并. 参考: 21. 合并两个有序链表.
-ListNode* SortList1(ListNode* head) { return SortList1(head, nullptr); }
-
-ListNode* SortList1(ListNode* head, ListNode* tail) {
-  if (head == nullptr) {
-    return head;
-  }
-  if (head->next == tail) {
-    head->next = nullptr;
-    return head;
-  }
-  ListNode *slow = head, *fast = head;
-  while (fast != tail) {
-    slow = slow->next;
-    fast = fast->next;
-    if (fast != tail) {
-      fast = fast->next;
-    }
-  }
-  ListNode* mid = slow;
-  return Merge(SortList1(head, mid), SortList1(mid, tail));
-}
-
 ListNode* Merge(ListNode* head1, ListNode* head2) {
   ListNode* dummyHead = new ListNode(0);
   ListNode *temp = dummyHead, *temp1 = head1, *temp2 = head2;
@@ -54,6 +57,26 @@ ListNode* Merge(ListNode* head1, ListNode* head2) {
   }
   return dummyHead->next;
 }
+
+// [head, tail)
+ListNode* SortList1(ListNode* head, ListNode* tail) {
+  if (head == nullptr) {
+    return head;
+  }
+  if (head->next == tail) {
+    head->next = nullptr;
+    return head;
+  }
+  ListNode *slow = head, *fast = head;
+  while (fast != tail && fast->next != tail) {
+    slow = slow->next;
+    fast = fast->next->next;
+  }
+  ListNode* mid = slow;
+  return Merge(SortList1(head, mid), SortList1(mid, tail));
+}
+
+ListNode* SortList1(ListNode* head) { return SortList1(head, nullptr); }
 
 // 2024/01/08: 直接抄答案.
 // 自底向上归并排序
@@ -101,4 +124,23 @@ ListNode* SortList2(ListNode* head) {
     }
   }
   return dummy_head->next;
+}
+
+int main(int argc, char* argv[]) {
+  ListNode* node1 = new ListNode(8);
+  ListNode* node2 = new ListNode(4);
+  ListNode* node3 = new ListNode(9);
+  ListNode* node4 = new ListNode(2);
+  ListNode* node5 = new ListNode(10);
+  node1->next = node2;
+  node2->next = node3;
+  node3->next = node4;
+  node4->next = node5;
+  SortList(node1);
+  delete node1;
+  delete node2;
+  delete node3;
+  delete node4;
+  delete node5;
+  return 0;
 }
