b88772074f68966e31e47e51f839c148b934f1f4
kushuaiming <xm2534911078@outlook.com>
2024-01-10 23:05:40 +0800

    docs: 46_permutations

--- backtracking/46_permutations.cc
+++ backtracking/46_permutations.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 
+// 2024/01/10: 直接抄答案.
 // [0, first - 1]是已经填过数的, [first, length - 1]是待填数的
 void Backtrack(std::vector<std::vector<int>>& result, std::vector<int>& output,
                int first, int length) {
@@ -19,6 +20,34 @@ std::vector<std::vector<int>> Permute(std::vector<int>& nums) {
   std::vector<std::vector<int>> result;
   Backtrack(result, nums, 0, nums.size());
   return result;
+}
+
+// 2024/01/10: 直接抄答案
+// 如果需要字典序排列, 则需要用is_visited数组进行记录是否已访问过.
+std::vector<std::vector<int>> res;
+std::vector<int> temp;
+std::vector<int> is_visited;
+void Backtrack2(const std::vector<int>& nums) {
+  if (temp.size() == nums.size()) {
+    res.push_back(temp);
+    return;
+  }
+  for (int i = 0; i < nums.size(); ++i) {
+    if (is_visited[i]) {
+      continue;
+    }
+    temp.push_back(nums[i]);
+    is_visited[i] = true;
+    Backtrack2(nums);
+    is_visited[i] = false;
+    temp.pop_back();
+  }
+}
+
+std::vector<std::vector<int>> Permute2(std::vector<int>& nums) {
+  is_visited.resize(nums.size(), 0);
+  Backtrack2(nums);
+  return res;
 }
 
 int main(int argc, char* argv[]) {
