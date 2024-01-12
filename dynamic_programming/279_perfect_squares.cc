559dda79f7e8ec35e1ebecc4d2a57e765575877b
kushuaiming <xm2534911078@outlook.com>
2024-01-12 17:52:51 +0800

    docs: 279_perfect_squares

--- dynamic_programming/279_perfect_squares.cc
+++ dynamic_programming/279_perfect_squares.cc
@@ -2,11 +2,13 @@
 
 #include "limits.h"
 
+// 2024/01/12: 自己实现
+
 int numSquares(int n) {
   std::vector<int> dp(n + 1);
   for (int i = 1; i < n + 1; ++i) {
     int minn = INT_MAX;
-    for (int j = 1; j * j <= i; ++j) {
+    for (int j = 1; j * j <= i; ++j) { // 无需开根号, 直接比较
       minn = std::min(minn, dp[i - j * j]);
     }
     dp[i] = minn + 1;
