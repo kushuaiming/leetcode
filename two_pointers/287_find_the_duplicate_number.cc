d33fa9d77856222cdc467c0c8ec5cf86ad4c6dd2
kushuaiming <xm2534911078@outlook.com>
2024-01-13 14:54:30 +0800

    feat: 287_find_the_duplicate_number

--- two_pointers/287_find_the_duplicate_number.cc
+++ two_pointers/287_find_the_duplicate_number.cc
@@ -20,3 +20,43 @@ int FindDuplicate(std::vector<int>& nums) {
   }
   return res;
 }
+
+// 2024/01/13: 直接看答案, 二进制的方法.
+int FindDuplicate2(std::vector<int>& nums) {
+  int n = nums.size(), ans = 0;
+  // 确定二进制下最高位是多少
+  int bit_max = 31;
+  while (!((n - 1) >> bit_max)) {
+    bit_max -= 1;
+  }
+  for (int bit = 0; bit <= bit_max; ++bit) {
+    int x = 0, y = 0;
+    for (int i = 0; i < n; ++i) {
+      if (nums[i] & (1 << bit)) {
+        x += 1;
+      }
+      if (i >= 1 && (i & (1 << bit))) {
+        y += 1;
+      }
+    }
+    if (x > y) {
+      ans |= 1 << bit;
+    }
+  }
+  return ans;
+}
+
+// 2024/01/13: 直接看答案, 快慢指针.
+int FindDuplicate3(std::vector<int>& nums) {
+  int slow = 0, fast = 0;
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow != fast);
+  slow = 0;
+  while (slow != fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+  return slow;
+}
