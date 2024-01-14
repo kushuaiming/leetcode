66f68bb8cccd3cdeff333ca284ebd684095df2d9
kushuaiming <xm2534911078@outlook.com>
2024-01-14 14:11:21 +0800

    docs: merge_two_sorted_list

--- linked_list/merge_two_sorted_list/23_merge_k_sorted_lists.cc
+++ linked_list/merge_two_sorted_list/23_merge_k_sorted_lists.cc
@@ -9,6 +9,7 @@ struct ListNode {
   ListNode(int val, ListNode* next) : val(val), next(next) {}
 };
 
+// 2024/01/14 自己实现, 注意优先队列的定义. TODO: 学习顺序合并和分治合并.
 // 2023/12/30 可以想出思路, 但是实现有细节上的错误.
 ListNode* MergeKLists(std::vector<ListNode*>& lists) {
   if (lists.empty()) {
