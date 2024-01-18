96ff38cfb79dcfd790145e83912a70d61b8dcd53
Ming Xu <ming.xu@allride.ai>
2024-01-18 19:33:23 +0800

    feat: 75_sort_colors

--- two_pointers/75_sort_colors.cc
+++ two_pointers/75_sort_colors.cc
@@ -1,5 +1,20 @@
 #include <vector>
 
+// 2024/01/18: 直接看答案, 选择此方法进行记忆.
+void SortColors(std::vector<int>& nums) {
+  int left = 0, right = nums.size() - 1;
+  for (int i = 0; i <= right; ++i) {
+    // 这里需要循环的原因是nums[right]是还没有遍历过的.
+    while (i <= right && nums[i] == 2) {
+      std::swap(nums[i], nums[right--]);
+    }
+    // 这里不需要循环的原因是nums[left]是已经遍历过的.
+    if (nums[i] == 0) {
+      std::swap(nums[i], nums[left++]);
+    }
+  }
+}
+
 // 2024/01/13: 自己实现双指针, 8min.
 void SortColors(std::vector<int>& nums) {
   int left = -1, right = nums.size();
