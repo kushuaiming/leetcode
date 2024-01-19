20d3279ddf504083e0ac0137099d71790b567703
Ming Xu <ming.xu@allride.ai>
2024-01-19 16:26:23 +0800

    docs: 309_best_time_to_buy_and_sell_stock_with_cooldown

--- dynamic_programming/buy_and_sell_stock/309_best_time_to_buy_and_sell_stock_with_cooldown.cc
+++ dynamic_programming/buy_and_sell_stock/309_best_time_to_buy_and_sell_stock_with_cooldown.cc
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 
 int MaxProfit(std::vector<int>& prices) {
@@ -6,17 +7,19 @@ int MaxProfit(std::vector<int>& prices) {
   }
 
   int n = prices.size();
-  // f[i][0]: 手上持有股票的最大收益
-  // f[i][1]: 手上不持有股票, 并且处于冷冻期中的累计最大收益, 第i天卖出股票了.
-  // f[i][2]: 手上不持有股票, 并且不在冷冻期中的累计最大收益, 第i天没有卖出股票.
-  std::vector<std::vector<int>> f(n, std::vector<int>(3));
-  f[0][0] = -prices[0];
+  // buy[i]: 手上持有股票的最大收益
+  // freeze[i]: 手上不持有股票, 并且处于冷冻期中的累计最大收益, 第i天卖出股票了.
+  // sell[i]: 手上不持有股票, 并且不在冷冻期中的累计最大收益, 第i天没有卖出股票.
+  std::vector<int> buy(n, 0);
+  std::vector<int> freeze(n, 0);
+  std::vector<int> sell(n, 0);
+  buy[0] = -prices[0];
   for (int i = 1; i < n; ++i) {
-    f[i][0] = std::max(f[i - 1][0], f[i - 1][2] - prices[i]);
-    f[i][1] = f[i - 1][0] + prices[i];
-    f[i][2] = std::max(f[i - 1][1], f[i - 1][2]);
+    buy[i] = std::max(buy[i - 1], sell[i - 1] - prices[i]);
+    freeze[i] = buy[i - 1] + prices[i];
+    sell[i] = std::max(freeze[i - 1], sell[i - 1]);
   }
-  return std::max(f[n - 1][1], f[n - 1][2]);
+  return std::max(freeze[n - 1], sell[n - 1]);
 }
 
 // 用状态机来解决复杂的状态转移问题
@@ -39,4 +42,10 @@ int MaxProfit2(const std::vector<int>& prices) {
     s2[i] = std::max(s2[i - 1], sell[i - 1]);
   }
   return std::max(sell[days - 1], s2[days - 1]);
+}
+
+int main(int argc, char* argv[]) {
+  std::vector<int> prices = {1, 2, 3, 0, 2};
+  std::cout << MaxProfit(prices) << std::endl;
+  return 0;
 }
