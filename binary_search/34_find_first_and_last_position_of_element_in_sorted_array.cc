e2e71efb3469690ea0c52a55e12a7d047facc075
kushuaiming <xm2534911078@outlook.com>
2024-01-21 15:14:30 +0800

    docs: 34_find_first_and_last_position_of_element_in_sorted_array

--- binary_search/34_find_first_and_last_position_of_element_in_sorted_array.cc
+++ binary_search/34_find_first_and_last_position_of_element_in_sorted_array.cc
@@ -2,6 +2,7 @@
 // 寻找left_index即为在数组中寻找第一个大于等于target的下标
 // 寻找right_index即为在数组中寻找第一个大于target的下标, 然后将下标减1
 // 在cpp标准库中有对应的lower_bound和upper_bound函数
+#include <algorithm>
 #include <iostream>
 #include <limits>
 #include <vector>
@@ -68,7 +69,21 @@ int upper_bound2(std::vector<int>& nums, int target) {
 std::vector<int> SearchRange(std::vector<int>& nums, int target) {
   int left_index = lower_bound(nums, target);
   int right_index = upper_bound(nums, target) - 1;
-  // 注意这里的判断顺序.
+  // 关键: 注意这里的判断顺序.
+  if (left_index <= right_index && right_index < nums.size() &&
+      nums[left_index] == target && nums[right_index] == target) {
+    return std::vector<int>{left_index, right_index};
+  }
+  return std::vector<int>{-1, -1};
+}
+
+// 2024/01/21: 自己调用相关库函数实现了一遍.
+// 注意一些特殊测试用例, 比如[[1]], 1; [[2, 2]], 2;
+std::vector<int> SearchRange(std::vector<int>& nums, int target) {
+  auto lower_bound = std::lower_bound(nums.begin(), nums.end(), target);
+  auto upper_bound = std::upper_bound(nums.begin(), nums.end(), target);
+  int left_index = std::distance(nums.begin(), lower_bound);
+  int right_index = std::distance(nums.begin(), upper_bound) - 1;
   if (left_index <= right_index && right_index < nums.size() &&
       nums[left_index] == target && nums[right_index] == target) {
     return std::vector<int>{left_index, right_index};
