354701780fcd821a9283083b8a8a4b15b4389052
Xu Ming <xm2534911078@outlook.com>
2026-01-10 18:13:19 +0800

    feat: 49_group_anagram.py

---
+++ data_structure/hash_table/49_group_anagram.py
@@ -0,0 +1,23 @@
+from typing import List
+
+# 2026/01/10 思路没问题, 实现上要注意 ord 的使用, 同时注意字典的 key 不能是 List, 只能是 Tuple.
+def word_letter_count(word: str) -> tuple:
+    word_letter_counts = [0] * 26
+    for letter in word:
+        word_letter_counts[ord(letter) - ord('a')] += 1
+    return tuple(word_letter_counts)
+
+def group_anagrams(strs: List[str]) -> List[List[str]]:
+    anagram_dict = {}
+    for word in strs:
+        counts = word_letter_count(word)
+        if counts not in anagram_dict:
+            anagram_dict[counts] = [word]
+        else:
+            anagram_dict[counts].append(word)
+    
+    return list(anagram_dict.values())
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+group_anagrams_result = group_anagrams(strs)
+print(group_anagrams_result)
