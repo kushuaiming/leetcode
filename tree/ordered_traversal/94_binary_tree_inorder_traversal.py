e2cd4ad749c9ddf960e492f6b07a7618da18732a
xum10 <xum10@xiaopeng.com>
2026-05-15 16:52:05 +0800

    94_binary_tree_inorder_traversal.py

---
+++ tree/ordered_traversal/94_binary_tree_inorder_traversal.py
@@ -0,0 +1,36 @@
+from typing import Optional, List
+from tree.tree_node import TreeNode, create_tree_node
+
+
+def inorder(root: Optional[TreeNode], res: List) -> None:
+    if root is None:
+        return
+    inorder(root.left, res)
+    res.append(root.val)
+    inorder(root.right, res)
+
+
+def inorder_traversal(root: Optional[TreeNode]) -> List[int]:
+    res = []
+    inorder(root, res)
+    return res
+
+
+# 2026/05/15: 直接看答案, 迭代版本.
+def inorder_traversal2(root: Optional[TreeNode]) -> List[int]:
+    res = []
+    stack = []
+    while root or len(stack) > 0:
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack[-1]
+        stack.pop()
+        res.append(root.val)
+        root = root.right
+    return res
+
+
+root = [1, None, 2, 3]
+root = create_tree_node(root)
+print(inorder_traversal2(root))
---
+++ tree/tree_node.py
@@ -0,0 +1,28 @@
+from collections import deque
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def create_tree_node(vals: List) -> Optional[TreeNode]:
+    if not vals or vals[0] is None:
+        return None
+    root = TreeNode(vals[0])
+    queue = deque([root])
+    i = 1
+    while queue and i < len(vals):
+        node = queue.popleft()
+        if i < len(vals) and vals[i] is not None:
+            node.left = TreeNode(vals[i])
+            queue.append(node.left)
+        i += 1
+        if i < len(vals) and vals[i] is not None:
+            node.right = TreeNode(vals[i])
+            queue.append(node.right)
+        i += 1
+    return root
