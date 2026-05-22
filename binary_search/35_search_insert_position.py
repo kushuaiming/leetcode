db85486b7fc88a1209a5ab76c86757d4773fc63b
xum10 <xum10@xiaopeng.com>
2026-05-22 16:42:05 +0800

    35_search_insert_position.py

---
+++ binary_search/35_search_insert_position.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+# 2026/05/22: 自己写出来.
+def search_insert(nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1  # [left, right]
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] < target:
+            left = mid + 1
+        elif nums[mid] > target:
+            right = mid - 1
+        else:
+            return mid
+
+    return left
+
+
+nums = [1, 3, 5, 6]
+target = -1
+print(search_insert(nums, target))
